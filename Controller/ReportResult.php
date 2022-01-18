25970b9b9e1d8eef2a6a09e6df5f344ea5be8f80
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-01-18 13:41:18 +0100

    - Actualizadas las traducciones. - Corregido bug al desplegar la tarjeta de ventas cuando no hay ventas.

--- Controller/ReportResult.php
+++ Controller/ReportResult.php
@@ -1,6 +1,20 @@
 <?php
 /**
- * Copyright (C) 2019-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * This file is part of Informes plugin for FacturaScripts
+ * Copyright (C) 2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace FacturaScripts\Plugins\Informes\Controller;
@@ -15,7 +29,6 @@ use FacturaScripts\Plugins\Informes\Lib\Informes\SalesResultReport;
 use FacturaScripts\Plugins\Informes\Lib\Informes\SummaryResultReport;
 
 /**
- *
  * @author Daniel Fernández Giménez <hola@danielfg.es>
  */
 class ReportResult extends Controller
