e3cf832c9d00e9f2dfb926b7644bc451107da5c4
Pablo Ferrandez Roca <pferrandezroca2@gmail.com>
2025-09-08 13:44:20 +0200

    añadidos tests para ReportBoard

---
+++ Test/main/ReportBoardTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace FacturaScripts\Test\Plugins;
+
+use FacturaScripts\Plugins\Informes\Model\Report;
+use FacturaScripts\Plugins\Informes\Model\ReportBoard;
+use FacturaScripts\Test\Traits\LogErrorsTrait;
+use PHPUnit\Framework\TestCase;
+
+final class ReportBoardTest extends TestCase
+{
+    use LogErrorsTrait;
+
+    public function testAddLineTrue(): void
+    {
+        $reportBoard = new ReportBoard();
+
+        $reportBoard->id = 9999;
+        $reportBoard->name = 'Test board';
+        $this->assertTrue($reportBoard->save());
+
+        $report = new Report();
+        $this->assertFalse($report->load('nobalance'));
+        $this->assertTrue($reportBoard->addLine($report));
+
+        $this->assertTrue($reportBoard->delete());
+    }
+
+    public function testAddLineFalse(): void
+    {
+        $reportBoard = new ReportBoard();
+
+        $reportBoard->id = 9998;
+        $reportBoard->name = 'Test board';
+        $this->assertTrue($reportBoard->save());
+
+        $report = new Report();
+        
+        $report->id = 999999;
+        $report->name = 'Test report';
+        $report->table = 'test_table';
+        $report->type = Report::DEFAULT_TYPE;
+        $this->assertTrue($report->save());
+
+        $report2 = new Report();
+        $report2->id = 999998;
+
+        $this->assertFalse($reportBoard->addLine($report2));
+        
+        $this->assertTrue($reportBoard->delete());
+        $this->assertTrue($report->delete());
+    }
+
+    public function testGetLines(): void
+    {
+        $reportBoard = new ReportBoard();
+
+        $this->assertIsArray($reportBoard->getLines());
+    }
+
+    public function testPrimaryDescriptionColumn(): void
+    {
+        $reportBoard = new ReportBoard();
+        $this->assertEquals('name', $reportBoard->primaryDescriptionColumn());
+    }
+
+    public function testTableName(): void
+    {
+        $reportBoard = new ReportBoard();
+        $this->assertEquals('reports_boards', $reportBoard->tableName());
+    }
+
+    protected function tearDown(): void
+    {
+        $this->logErrors();
+    }
+}
